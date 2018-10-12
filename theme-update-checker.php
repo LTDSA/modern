f2710a17c3a1c351d41bb76d8d2f2d9b58b2145c
LTDSA <36990008+LTDSA@users.noreply.github.com>
2018-10-12 13:25:36 +0800

    Update theme-update-checker.php

--- theme-update-checker.php
+++ theme-update-checker.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Theme Update Checker Library 1.2
+ * 主题更新检查程序库 1.2
  * http://w-shadow.com/
  * 
- * Copyright 2012 Janis Elsts
+ * 版权 2012 Janis Elsts
  * Licensed under the GNU GPL license.
  * http://www.gnu.org/licenses/gpl.html
  */
